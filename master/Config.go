5789c9a2e3c6636c2c219b325ebe5d834b027a7a
万仁亮 <wanrenliang@oneniceapp.com>
2020-02-07 17:35:03 +0800

    all

--- master/Config.go
+++ master/Config.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 )
 
+// 程序配置
 type Config struct {
 	ApiPort               int      `json:"apiPort"`
 	ApiReadTimeout        int      `json:"apiReadTimeout"`
@@ -26,6 +27,7 @@ func InitConfig(filename string) (err error) {
 		content []byte
 		conf    Config
 	)
+
 	// 1.把配置文件读进来
 	if content, err = ioutil.ReadFile(filename); err != nil {
 		return
@@ -38,5 +40,6 @@ func InitConfig(filename string) (err error) {
 
 	// 3.赋值单例
 	G_config = &conf
+
 	return
 }
--- master/main/master.go
+++ master/main/master.go
@@ -41,7 +41,17 @@ func main() {
 		goto ERR
 	}
 
-	// 任务管理器
+	// 初始化服务发现模块
+	if err = master.InitWorkerMgr(); err != nil {
+		goto ERR
+	}
+
+	// 日志管理器
+	if err = master.InitLogMgr(); err != nil {
+		goto ERR
+	}
+
+	//  任务管理器
 	if err = master.InitJobMgr(); err != nil {
 		goto ERR
 	}
